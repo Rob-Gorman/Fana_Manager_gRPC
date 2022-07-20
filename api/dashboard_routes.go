dbb00d764ee365703f51f171841eb878f60da77a
rjg <gorman.rob.j@gmail.com>
2022-07-20 01:00:12 -0400

    /api

--- api/dashboard_routes.go
+++ api/dashboard_routes.go
@@ -1,12 +1,14 @@
 package api
 
-func (s *Server) dashboardRoutes() {
-	s.HandleFunc("/flags", s.H.GetAllFlags).Methods("GET")
-	s.HandleFunc("/flags", s.H.CreateFlag).Methods("POST")
-	s.HandleFunc("/flags/{id}", s.H.GetFlag).Methods("GET")
+import "github.com/gorilla/mux"
+
+func (s *Server) dashboardRoutes() *mux.Route {
+	s.HandleFunc("/api/flags", s.H.GetAllFlags).Methods("GET")
+	s.HandleFunc("/api/flags", s.H.CreateFlag).Methods("POST")
+	s.HandleFunc("/api/flags/{id}", s.H.GetFlag).Methods("GET")
 	// s.HandleFunc("/flags/{id}", s.H.UpdateFlag).Methods("PATCH")
-	s.HandleFunc("/audiences", s.H.GetAllAudiences).Methods("GET")
-	s.HandleFunc("/audiences/{id}", s.H.GetAudience).Methods("GET")
-	s.HandleFunc("/attributes", s.H.GetAllAttributes).Methods("GET")
-	s.HandleFunc("/attributes", s.H.CreateAttribute).Methods("POST")
+	s.HandleFunc("/api/audiences", s.H.GetAllAudiences).Methods("GET")
+	s.HandleFunc("/api/audiences/{id}", s.H.GetAudience).Methods("GET")
+	s.HandleFunc("/api/attributes", s.H.GetAllAttributes).Methods("GET")
+	s.HandleFunc("/api/attributes", s.H.CreateAttribute).Methods("POST")
 }
--- api/server.go
+++ api/server.go
@@ -28,34 +28,3 @@ func (s *Server) routes() {
 	s.dashboardRoutes()
 	// s.providerRoutes()
 }
-
-// func (s *Server) getMongoData() http.HandlerFunc {
-// 	query := configs.ConnectClient()
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Content-Type", "application/json")
-// 		if err := json.NewEncoder(w).Encode(query); err != nil {
-// 			http.Error(w, err.Error(), http.StatusInternalServerError)
-// 			return
-// 		}
-// 	}
-// }
-
-// func (s *Server) getFlagData() http.HandlerFunc {
-// 	return func(w http.ResponseWriter, r *http.Request) {
-// 		w.Header().Set("Content-Type", "application/json")
-// 		if err := json.NewEncoder(w).Encode(*(s.Ruleset)); err != nil {
-// 			http.Error(w, err.Error(), http.StatusInternalServerError)
-// 			return
-// 		}
-// 	}
-// }
-
-// func GetRuleset(db *mongo.Database) (results []bson.M) {
-// 	ctx, _ := utils.StandardContext()
-// 	flags := db.Collection("flags")
-// 	query, err := flags.Find(ctx, bson.D{})
-// 	utils.HandleErr(err, "Cannot query the collection")
-
-// 	query.All(ctx, &results)
-// 	return results
-// }
